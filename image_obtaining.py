84e5062773e30d42c0a06532986902fb17047949
lichunjiang19960224 <54852331+lichunjiang19960224@users.noreply.github.com>
2021-01-07 17:20:16 +0800

    Update and rename mp4_shen.py to image_obtaining.py

--- image_obtaining.py
+++ image_obtaining.py
@@ -1,20 +1,23 @@
 import numpy as np
 import cv2
+import os, shutil
 # 读取视频文件
 cap = cv2.VideoCapture('1.mp4')
 # 或者电影每秒的帧数
+dir = 'Input//'
 fps = cap.get(cv2.CAP_PROP_FPS)
 # 判断视频是否一直打开
 i = 0
-txtx = open('3MP4/test.txt','w')
+if os.path.exists(dir):
+    shutil.rmtree(dir)
+os.mkdir(dir)
+txtx = open(dir + 'test.txt','w')
 while (cap.isOpened()):
     success, frame = cap.read()
     if success == False:
         break
-    cv2.imwrite('3MP4/'+'0'*(4-len('%d'%i))+'%d'%i+'.png',frame)
+    cv2.imwrite(dir+'0'*(4-len('%d'%i))+'%d'%i+'.png',frame)
     i += 1
-    txtx.write('3MP4/'+'0'*(4-len('%d'%i))+'%d'%i+'.png ')
-    txtx.write('3MP4/' + '0' * (4 - len('%d' % i)) + '%d' % i + '.png')
+    txtx.write(dir+'0'*(4-len('%d'%i))+'%d'%i+'.png ')
+    txtx.write(dir + '0' * (4 - len('%d' % i)) + '%d' % i + '.png')
     txtx.write('\n')
-# 清除缓存退出
-cv2.destroyAllWindows()
